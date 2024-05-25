51dc3125d2c48c81e5ee02594dc71d134d88b04c
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-05-25 17:09:23 +0200

    Initial commit

---
+++ src/main/java/com/example/test_extract_aot/TestExtractAotApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_extract_aot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestExtractAotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestExtractAotApplication.class, args);
+	}
+
+}
